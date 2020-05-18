6b0aa47a7a4ae72d5d3c6737a5b673940ea14898
Elad Jacobovitz <62474541+Eladjacobovitz@users.noreply.github.com>
2020-05-18 08:38:33 +0300

    Update MazeTest.java

--- src/test/MazeTest.java
+++ src/test/MazeTest.java
@@ -12,19 +12,19 @@ public class MazeTest {
     *
     * @param args command line arguments - name of file to process.
     */
-   public static void main (String[] args) {
-	  // we will  run the program on a maze that has no solution.
-	  // We expect to see a negative answer on whether the maze has a solution and in addition, 
-	  // we would like to see the maze component number when each component is painted a different color
-	  Maze maze = new Maze ("maze4.png");
+   public static void main (String[] args) { 
+      // we will  run the program on a maze that has no solution.
+      // We expect to see a negative answer on whether the maze has a solution and in addition, 
+      // we would like to see the maze component number when each component is painted a different color
+      Maze maze = new Maze ("maze4.png");
       System.out.println (maze.mazeHasSolution());
       System.out.println ("Number of components: " + maze.getNumComponents());
       DisplayImage compImg = maze.getComponentImage();
       compImg.show();
       
       // we will  run the program on a maze that has solution.
-   	  // We expect to see a positive answer on whether the maze has a solution and in addition, 
-   	  // we would like to see the maze component number when each component is painted a different color
+      // We expect to see a positive answer on whether the maze has a solution and in addition, 
+      // we would like to see the maze component number when each component is painted a different color
       maze = new Maze ("maze5.png");
       System.out.println (maze.mazeHasSolution());
       System.out.println ("Number of components: " + maze.getNumComponents());
